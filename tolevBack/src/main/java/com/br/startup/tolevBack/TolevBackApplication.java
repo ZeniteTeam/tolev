22a49c564982fde61e7fc2a096f2bc7307b74f0a
Patricia Pederzini <derickpps@gmail.com>
2026-05-08 18:50:22 -0300

    init project

---
+++ tolevBack/src/main/java/com/br/startup/tolevBack/TolevBackApplication.java
@@ -0,0 +1,13 @@
+package com.br.startup.tolevBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolevBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolevBackApplication.class, args);
+	}
+
+}
---
+++ tolevBack/src/test/java/com/br/startup/tolevBack/TolevBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.startup.tolevBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TolevBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
